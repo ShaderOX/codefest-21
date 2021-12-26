9172ff3b7914f63c85b8414e6ed44d598b1185e4
green-mint <saad.521509@gmail.com>
2021-12-26 11:05:29 +0500

    DJNwkjefnj

--- backend/actions/actions.py
+++ backend/actions/actions.py
@@ -1,27 +1,49 @@
-# This files contains your custom actions which can be used to run
-# custom Python code.
-#
-# See this guide on how to implement these action:
-# https://rasa.com/docs/rasa/custom-actions
+import datetime as dt
+import json
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
 
 
-# This is a simple example for a custom action which utters "Hello World!"
+class ActionShowCategories(Action):
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+    def name(self) -> Text:
+        return "action_show_categories"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        
+        with open('data/categories.txt', 'r') as f:
+            categories = f.read()
+            dispatcher.utter_message(text=f"{categories}. Which one do you want to explore?")
+
+        return []
+
+class ActionShowProductsInCategory(Action):
+    
+        def name(self) -> Text:
+            return "action_show_products_in_category"
+    
+        def run(self, dispatcher: CollectingDispatcher,
+                tracker: Tracker,
+                domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+            
+            category = tracker.get_slot('product_category')
+            return_str = " "
+            found_cat = False
+            with open(f'data/products.json', 'r') as f:
+                products = json.load(f)
+                for product in products["products"]:
+                    # ASAD
+                    if product["category"].lower() == category.lower():
+                        found_cat = True
+                        return_str += f"{product['name']} {product['price']}\n"
+            if found_cat:
+                dispatcher.utter_message(text=return_str)
+            else:
+                dispatcher.utter_message(text="Sorry, We don't have any products in this category.")
+            return []
+
+

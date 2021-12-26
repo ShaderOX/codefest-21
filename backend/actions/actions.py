df4ed19d5562d81ca3537bbe18471bb488d99479
green-mint <saad.521509@gmail.com>
2021-12-26 13:42:04 +0500

    did to some extent

--- backend/actions/actions.py
+++ backend/actions/actions.py
@@ -21,16 +21,16 @@ class ActionShowCategories(Action):
 
         return []
 
-class ActionShowProductsInCategory(Action):
+class ActionShowProductsInCategoryFromSlot(Action):
     
         def name(self) -> Text:
-            return "action_show_products_in_category"
+            return "action_show_products_in_category_from_slot"
     
         def run(self, dispatcher: CollectingDispatcher,
                 tracker: Tracker,
                 domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-            
             category = tracker.get_slot('product_category')
+            
             return_str = " "
             found_cat = False
             with open(f'data/products.json', 'r') as f:
@@ -46,4 +46,55 @@ class ActionShowProductsInCategory(Action):
                 dispatcher.utter_message(text="Sorry, We don't have any products in this category.")
             return []
 
+class ActionShowProductsInCategoryFromEntity(Action):
+    
+        def name(self) -> Text:
+            return "action_show_products_in_category_from_entity"
+    
+        def run(self, dispatcher: CollectingDispatcher,
+                tracker: Tracker,
+                domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+            category = tracker.get_slot('product_category')
+            
+            return_str = " "
+            found_cat = False
+            with open(f'data/products.json', 'r') as f:
+                products = json.load(f)
+                for product in products["products"]:
+                    
+                    if product["category"].lower() == category.lower():
+                        print(product)
+                        found_cat = True
+                        return_str += f"{product['name']} {product['price']}\n"
+            if found_cat:
+                print(return_str)
+                dispatcher.utter_message(text=return_str)
+            else:
+                dispatcher.utter_message(text="Sorry, We don't have any products in this category.")
+            return []
+
+class ActionShowProductById(Action):
+
+    def name(self) -> Text:
+        return "action_show_product_by_id"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        product_id = tracker.latest_message['entities'][0]['value']
+
+        print(product_id)
+        return_str = " "
+        found_product = False
+        with open(f'data/products.json', 'r') as f:
+            products = json.load(f)
+            for product in products["products"]:
+                if product["id"] == product_id:
+                    found_product = True
+                    return_str += f"{product['name']} {product['price']}\n"
+        if found_product:
+            dispatcher.utter_message(text=return_str)
+        else:
+            dispatcher.utter_message(text="Sorry, We don't have any products with this id.")
+        return []
 
